dafa04d7f03c37078d3eff79d3b41b8f8cd8ae75
Ceejay Gabrang <71625955+CEGA22@users.noreply.github.com>
2021-12-22 23:08:43 +0800

    Added sectionsHandled controller

---
+++ EZWebServices/Controllers/SectionsHandledController.cs
@@ -0,0 +1,24 @@
+﻿using EZWebServices.Models;
+using EZWebServices.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace EZWebServices.Controllers
+{
+    public class SectionsHandledController : ApiController
+    {
+        // GET: SectionsHandled
+        [AcceptVerbs("GET", "POST")]
+        [HttpGet]
+        [Route("api/lga/sectionsHandled/get_all/{ID}")]
+        public IEnumerable<SectionsHandled> GetSectionsHandled(int ID)
+        {
+            var classRecord = new SectionsHandled();
+            return classRecord.GetSectionsHandled(ID);
+        }
+    }
+}
---
+++ EZWebServices/Models/SectionsHandled.cs
@@ -0,0 +1,62 @@
+﻿using EZWebServices.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EZWebServices.Models
+{
+    public class SectionsHandled
+    {
+        public int ID { get; set; }
+
+        public int TeacherID { get; set; }
+
+        public int GradeLevelID { get; set; }
+
+        public List<SectionsHandled> GetSectionsHandled(int ID)
+
+        {
+            var listReturn = new List<SectionsHandled>();
+
+            using (var cn = new SqlConnection(ConnectionHelper.LGAConnection()))
+            {
+                cn.Open();
+                var cmd = cn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM SectionsHandled WHERE Teacher = @TeacherID";                
+                cmd.Parameters.AddWithValue("@TeacherID", ID);
+                var dr = cmd.ExecuteReader();
+                listReturn = PopulateReturnList(dr);
+            }         
+            return listReturn;
+        }
+
+        public List<SectionsHandled> PopulateReturnList(SqlDataReader dr)
+        {
+
+            try
+            {
+                var listReturn = new List<SectionsHandled>();
+
+                while (dr.Read())
+                {
+
+                    listReturn.Add(new SectionsHandled
+                    {
+                        ID = int.Parse(dr["ID"].ToString()),
+                        TeacherID = int.Parse(dr["Teacher"].ToString()),
+                        GradeLevelID = int.Parse(dr["Gradelevel"].ToString())
+                    });
+                }
+
+                return listReturn;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+    }
+}
